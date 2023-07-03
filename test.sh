ec8ac9422fefd8e4bb7b76b9b229dcdc25253417
ANCH <39931153+AyamNubCh@users.noreply.github.com>
2023-07-03 21:25:09 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,14 +5,14 @@ ENDCOLOR="\e[0m"
 clear
 echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
 sleep 1
-if [ -f "AnchProxy" ]; then
+if [ -f "LuckyProxy" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
-    rm AnchProxy
+    rm LuckyProxy
     sleep 1
     echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/AyamNubCh/AnchProxy/raw/main/AnchProxy
+wget -q https://github.com/AyamNubCh/AnchProxy/raw/main/LuckyProxy
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
 echo -e "${GREEN}Execute proxy with this command: ./proxy${ENDCOLOR}"
-chmod +x AnchProxy
+chmod +x LuckyProxy
